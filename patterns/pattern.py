24abd3a63b7b8a6fce66549200f47b7b0bd91d07
Punidhasree <punidhasreesekar@gmail.com>
2026-08-24 19:52:29 +0530

    new

--- patterns/pattern.py
+++ patterns/pattern.py
@@ -4,10 +4,41 @@ def pattern1(n):
             print('*',end=' ')
         print()
 
-
+#pattern2
     for i in range(n):
         for j in range(i+1):
             print('*',end=' ')
+        print()
+#pattern3
+    for i in range(n):
+            for j in range(i+1):
+                print(i+1,end=' ')
+            print()
+ #pattern4   
+    for i in range(n):
+        
+        for j in range(i+1):
+            print(j+1,end=' ')
+                
+        print()
+#pattern5
+    for i in range(n):
+        for j in range(n-i):
+            print("*",end=' ')
+        print()
+#pattern6
+    for i in range(n):
+        for j in range(n-i):
+            print(j+1,end=' ')
+        print()
+#pattern7   
+    for i in range(n):
+        for j in range(n-i):
+            print(" ",end=' ')
+        for _ in range(2*i+1):
+            print("*",end=' ')
+        for _ in range(n-i):
+            print(" ",end=' ')
         print()
 
 n=5
